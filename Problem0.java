3106397fd1311336d31745130fc4de260b595936
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-09-10 11:22:57 -0700

    sdfsdf

--- Problem0.java
+++ Problem0.java
@@ -2,6 +2,9 @@ public class Problem0
 {
   public static void main(String[] args)
   {
-    // insert code here
+   double value = 5; //Ints are smaller so if you store a 5 into a double, java turns a 5 into 5.0
+   System.out.println(value);
+   int valuestore = 5;
+   System.out.println(valuestore);
   }
 }
